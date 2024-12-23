c83e94656bc09cdf10482d7ee5b27248adab9a33
spatten <scott@fossa.com>
2024-12-23 12:04:18 -0800

    get rid off console.log

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,14 @@
 import * as vscode from 'vscode';
 
-const leadingSpacesMatcher = /^(\s*)/;
+const leadingSpacesMatcher = /^\s*/;
 
-const trim = (selected: string): string => {
+export const trim = (selected: string): string => {
 	const lines = selected.split("\n");
 	const leadingSpaceCount = lines.reduce((acc, currentLine) => {
 		const match = currentLine.match(leadingSpacesMatcher);
-		const numLeading = match && match.length >= 2 ? match[1].length : 0;
-		console.log(`numLeading: ${numLeading}, line: '${currentLine}`);
+		const numLeading = match && match.length >= 1 ? match[0].length : 0;
 		return (acc === -1 || numLeading < acc) ? numLeading : acc;
 	}, -1);
-	console.log(`leading space count: ${leadingSpaceCount}`);
 	const spaceRegex = RegExp(`^${' '.repeat(leadingSpaceCount)}`);
 	const fixed = lines.map(l => l.replace(spaceRegex, ''));
 	return fixed.join("\n");
